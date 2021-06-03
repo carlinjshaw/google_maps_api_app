dd0f547fb53790a0fe78043a02d448e5f4691049
Kayla Dombrosky <kdombroskyy@live.com>
2021-06-03 18:53:40 -0400

    add kayla test api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,21 @@
 // .catch(err => {
 // 	console.error(err);
 // });
+
+
+
+
+// --- from kayla's rapidAPI account --- 
+fetch("https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&language=en&radius=150&type=cafe", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "9b836ebcfamsh9bd18d25f72ad34p19e628jsn460c57bf97e3",
+		"x-rapidapi-host": "trueway-places.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
