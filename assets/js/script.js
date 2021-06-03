fb9b9371334496e1e36b36aac015a93699347348
odupree1988 <odupree1988@gmail.com>
2021-06-02 22:25:27 -0400

    minor changes to css added jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
-// fetch("https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&language=en&radius=150&type=cafe", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "54ce5bf2cdmshd88304cae69d7fbp1de188jsn1610517fe23b",
-// 		"x-rapidapi-host": "trueway-places.p.rapidapi.com"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&language=en&radius=150&type=cafe",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "54ce5bf2cdmshd88304cae69d7fbp1de188jsn1610517fe23b",
+		"x-rapidapi-host": "trueway-places.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
