3b68036fe505049721187e24c45fdaa5faf0d7ef
odupree1988 <odupree1988@gmail.com>
2021-06-02 22:27:28 -0400

    commented out js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&language=en&radius=150&type=cafe",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "54ce5bf2cdmshd88304cae69d7fbp1de188jsn1610517fe23b",
-		"x-rapidapi-host": "trueway-places.p.rapidapi.com"
-	}
-};
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&language=en&radius=150&type=cafe",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "54ce5bf2cdmshd88304cae69d7fbp1de188jsn1610517fe23b",
+// 		"x-rapidapi-host": "trueway-places.p.rapidapi.com"
+// 	}
+// };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
