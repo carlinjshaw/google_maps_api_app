b19cd9019e9ed5db5b176af13dadfbcb055eb06c
Kayla Dombrosky <kdombroskyy@live.com>
2021-06-09 15:48:41 -0400

    places api to html functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,87 @@
 var userLocation = {};
 
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function (response) {
-    //  placesApi(response.coords.latitude, response.coords.longitude);
-    userLocation.lat = response.coords.latitude;
-    userLocation.lon = response.coords.longitude;
-  });
-  console.log(userLocation);
-}
+//user location
+$("#location-btn").on("click", function (e) {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (response) {
+        //  placesApi(response.coords.latitude, response.coords.longitude);
+        userLocation.lat = response.coords.latitude;
+        userLocation.lon = response.coords.longitude;
+    });
+      //console.log(userLocation);
+    }
+});
 
-function placesApi(lat, lon) {
-  console.log(userLocation.lat, userLocation.lon);
-  var currentLat = lat;
-  var currentLon = lon;
-  fetch(
+
+//on click function to gather user data
+$("#submit").on("click", function (e) {
+    var userDistance = $("input:radio[name=distance]:checked").val();
+    var userMethod = $("input:radio[name=travel-method]:checked").val();
+    var userDestination = $("input:radio[name=location-type]:checked").val();
+
+        // package variables to send 
+    var userLat = userLocation.lat;
+    var userLon = userLocation.lon;
+    placesApi(userDistance, userMethod, userDestination, userLat, userLon);
+});
+
+
+//function placesApi(location, distance, method, destinationType) {
+function placesApi(userDistance, userMethod, userDestination, userLat, userLon) {
+fetch(
     "https://api.allorigins.win/raw?url=" +
-      encodeURIComponent(
+    encodeURIComponent(
         "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-          lat +
-          "," +
-          lon +
-          "&radius=100000&type=restaurant&key=AIzaSyBNemHqQ_a0mlEfgAo0C2IZN3hwCYT4RDo"
-      )
-  ).then(function (response) {
-    console.log(response);
+            userLat +
+            "," +
+            userLon +
+            "&radius=" +
+            userDistance +
+            "&type=" +
+            userDestination +
+            "&key=AIzaSyBNemHqQ_a0mlEfgAo0C2IZN3hwCYT4RDo"
+    )
+).then(function (response) {
     if (response.ok) {
-      response.json().then(function (data) {
+        response.json().then(function (data) {
         console.log(data);
+        var randomResults = data.results[Math.floor(Math.random() * data.results.length)];
+        console.log(randomResults);
 
-        var searchResults =
-          data.results[Math.floor(Math.random() * data.results.length)];
-        console.log(searchResults);
+        var locationLat = randomResults.geometry.location.lat;
+        var locationLng = randomResults.geometry.location.lng;
 
-        var locationLat = searchResults.geometry.location.lat;
-        var locationLng = searchResults.geometry.location.lng;
-
-        distanceMatrixApi(locationLat, locationLng, currentLat, currentLon);
-      });
+        distanceMatrixApi(locationLat, locationLng, userLat, userLon, userMethod);
+        });
     }
-  });
+});
 }
 
-function distanceMatrixApi(lat, lng, userLat, userLon) {
-  fetch(
+// Distance Matrix API
+function distanceMatrixApi(locationLat, locationLng, userLat, userLon, userMethod) {
+    //console.log(locationLat, locationLng, userLat, userLon, userMethod);
+    fetch(
     "https://api.allorigins.win/raw?url=" +
-      encodeURIComponent(
-        "https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyBjep3oamseN_xrez7jazTRvj92qBnDqFI&origins=" +
-          userLat +
-          "," +
-          userLon +
-          "&destinations=" +
-          lat +
-          "," +
-          lng
-      )
-  ).then(function (response) {
+        encodeURIComponent(
+        "https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyBjep3oamseN_xrez7jazTRvj92qBnDqFI&units=imperial&origins=" +
+            userLat +
+            "," +
+            userLon +
+            "&destinations=" +
+            locationLat +
+            "," +
+            locationLng +
+            "&mode=" + userMethod
+        )
+).then(function (response) {
     if (response.ok) {
-      response.json().then(function (data) {
+        response.json().then(function (data) {
         console.log(data);
-      });
+    });
     }
-  });
+});
 }
+
+
+
+//data.rows[0].elements[0].distance (use to compare to make sure the response returned is within distance parameters)
