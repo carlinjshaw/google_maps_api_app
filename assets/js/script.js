5e24ec7b1140bc9f5d2d4e15cc36718e22f2b721
Elba Core <ecore@pinnaclehomecare.local>
2021-06-08 20:59:10 -0400

    cleared out pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
-// const placesApi =
-// const distanceMatrixApi =
-
-// fetch all places with the type restaurant, with a keyword red, within a 60 mile radius of orlando
-// fetch('https://api.allorigins.win/raw?url=' +
-// encodeURIComponent('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.5383,-81.3792&radius=100000&type=restaurant&key=AIzaSyBNemHqQ_a0mlEfgAo0C2IZN3hwCYT4RDo'))
-// .then(res => res.json())
-// .then(console.log);
-
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(function (response) {
+  placesApi(response.coords.latitude, response.coords.longitude);
+    // userLocation.push(response.coords.latitude)
+    });
+  }
+  // console.log(userLocation)
+  
 function placesApi(lat, lon) {
   console.log(lat, lon);
-  var currentLat = lat
-  var currentLon = lon
+  var currentLat = lat;
+  var currentLon = lon;
   fetch(
     "https://api.allorigins.win/raw?url=" +
       encodeURIComponent(
@@ -25,15 +24,15 @@ function placesApi(lat, lon) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data);
-  
-        
-        var searchResults = data.results[Math.floor(Math.random() * data.results.length)];
-        console.log(searchResults)
-        
-        var locationLat = searchResults.geometry.location.lat
-        var locationLng = searchResults.geometry.location.lng
-        
-        distanceMatrixApi(locationLat, locationLng, currentLat, currentLon)
+
+        var searchResults =
+          data.results[Math.floor(Math.random() * data.results.length)];
+        console.log(searchResults);
+
+        var locationLat = searchResults.geometry.location.lat;
+        var locationLng = searchResults.geometry.location.lng;
+
+        distanceMatrixApi(locationLat, locationLng, currentLat, currentLon);
       });
     }
   });
@@ -44,9 +43,13 @@ function distanceMatrixApi(lat, lng, userLat, userLon) {
     "https://api.allorigins.win/raw?url=" +
       encodeURIComponent(
         "https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyBjep3oamseN_xrez7jazTRvj92qBnDqFI&origins=" +
-          userLat + "," + userLon
-          + "&destinations=" 
-          + lat + "," + lng
+          userLat +
+          "," +
+          userLon +
+          "&destinations=" +
+          lat +
+          "," +
+          lng
       )
   ).then(function (response) {
     if (response.ok) {
@@ -56,35 +59,3 @@ function distanceMatrixApi(lat, lng, userLat, userLon) {
     }
   });
 }
-
-// var lat = "";
-// var lon = "";
-// var type = ""; //restaurant,
-// var keyword = "";  //"red",
-
-// function to fetch data in a 60mile radius using input variables
-// var fetchLocationData = function() {
-// 	fetch('https://api.allorigins.win/raw?url=' +
-// 	encodeURIComponent('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + '','' + lon + '&radius=100000&type=' + type + '&keyword=' + keyword + '&key=AIzaSyBNemHqQ_a0mlEfgAo0C2IZN3hwCYT4RDo'))
-// 	.then(res => res.json())
-// 	.then(console.log);
-// 	};
-
-// fetch('')
-// .then(function(response) {
-// 	response.json().then(function(data) {
-// 		console.log(data);
-// 	});
-// });
-
-// function userLocation(){
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function (response) {
-    console.log(response);
-    placesApi(response.coords.latitude, response.coords.longitude);
-  }); 
-}
-// }
-
-// userLocation()
-distanceMatrixApi()
