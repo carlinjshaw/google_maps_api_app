6017a1526da9bbd41245499b0c8759ab0f358f0d
Elba Core <ecore@pinnaclehomecare.local>
2021-06-11 06:27:31 -0400

    refactored passed data into objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 var savedUserInput = [];
 var userLocation = {};
 // DOM Elements
-var placeNameEl = document.querySelector("#place-name");
+// var placeNameEl = document.querySelector("#place-name");
 var imgEl = document.querySelector("#img-el");
-var addressEl = document.querySelector("#address-el");
-var distanceEl = document.querySelector("#distance");
-var timeEl = document.querySelector("#travel-time");
-var statusEl = document.querySelector("#status-el");
-var mapEl = document.querySelector("#map-content");
-var iconEl = document.querySelector("#place-icon")
+// var addressEl = document.querySelector("#address-el");
+// var distanceEl = document.querySelector("#distance");
+// var timeEl = document.querySelector("#travel-time");
+// var statusEl = document.querySelector("#status-el");
+// var mapEl = document.querySelector("#map-content");
+// var iconEl = document.querySelector("#place-icon")
 var storedPlacesEl = document.querySelector("#storage-row");
 
 function updateLocalStorage(key, data) {
@@ -47,54 +47,63 @@ function checkHistory() {
 
 // function to display content to page
 var displayContent = function (placeObject, infObject) {
-  placeNameEl.innerHTML = placeObject.name;
-  addressEl.innerText = placeObject.address;
-  distanceEl.innerText = infObject.distance;
-  timeEl.innerText = infObject.time;
+  $("#place-name").text(placeObject.name);
+  $("#address-el").text(placeObject.address);
+  $("#distance").text(infObject.distance);
+  $("#travel-time").text(infObject.time);
   if (placeObject.status) {
-    statusEl.innerText = "Open Now";
+    $("#status-el").text("Open Now");
   } else {
-    statusEl.innerText = "Closed";
-    }
-    console.log(placeObject.type);
-    if(placeObject.type === "restaurant") {
-        iconEl.innerText = "local_dining";
-        if(iconEl.classList.contains("red-text")) {
-            iconEl.classList.remove("red-text");
-        }
-    }
-    if(placeObject.type === "point-of-interest") {
-        iconEl.innerText = "room";
-        iconEl.classList.add("red-text");
-    }
+    $("#status-el").text("Closed");
+  }
+  console.log(placeObject.type);
+  if (placeObject.type === "restaurant") {
+    $("#place-icon").text("local_dining");
+    // iconEl.innerText = "local_dining";
+  }
+  if (placeObject.type === "point-of-interest") {
+    $("#place-icon").text("room");
+    // iconEl.innerText = "room";
+    $("#place-icon").addClass("large material-icons red-text");
+    // iconEl.className = "large material-icons red-text"
+  }
 };
 
 // display map
 var fetchStatic = function (
-  originLat,
-  originLon,
-  zoomValue,
-  destinationLat,
-  destinationLon
+  ranLocation,
+  locationObject,
+  zoomValue
+  // originLat,
+  // originLon,
+  // zoomValue,
+  // destinationLat,
+  // destinationLon
 ) {
   var staticUrl =
     "https://maps.googleapis.com/maps/api/staticmap?center=" +
-    originLat +
+    locationObject.lat +
+    // originLat +
     "," +
-    originLon +
+    locationObject.lon +
+    // originLon +
     "&zoom=" +
     zoomValue +
     "&size=480x480&maptype=roadmap&markers=color:green%7Clabel:Start%7C" +
-    originLat +
+    locationObject.lat +
+    // originLat +
     "," +
-    originLon +
+    locationObject.lon +
+    // originLon +
     "&markers=color:red%7Clabel:Finish%7C" +
-    destinationLat +
+    ranLocation.lat +
+    // destinationLat +
     "," +
-    destinationLon +
+    ranLocation.lng +
+    // destinationLon +
     "&key=AIzaSyA76IoInowLeKlfuTlf0yYHVH95eZAz4mg";
   console.log(staticUrl);
-  mapEl.innerHTML = "<img src='" + staticUrl + "'/ >";
+  $("#map-content").html("<img src='" + staticUrl + "'/ >");
 };
 
 // fetch photo from places
@@ -123,26 +132,29 @@ var fetchStatic = function (
 
 // DISTANCE MATRIX API
 function distanceMatrixApi(
-  locationLat,
-  locationLng,
-  userLat,
-  userLon,
-  userMethod,
+  ranLocation,
+  locationObject,
   placeObject
+  // locationLat,
+  // locationLng,
+  // userLat,
+  // userLon,
+  // userMethod,
+  // placeObject
 ) {
   fetch(
     "https://api.allorigins.win/raw?url=" +
       encodeURIComponent(
         "https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyBjep3oamseN_xrez7jazTRvj92qBnDqFI&units=imperial&origins=" +
-          userLat +
+          locationObject.lat +
           "," +
-          userLon +
+          locationObject.lon +
           "&destinations=" +
-          locationLat +
+          ranLocation.lat +
           "," +
-          locationLng +
+          ranLocation.lng +
           "&mode=" +
-          userMethod
+          locationObject.method
       )
   )
     .then(function (response) {
@@ -160,26 +172,25 @@ function distanceMatrixApi(
           } else {
             zoomValue = 10;
           }
-          
-            // on click save btn to update local storage
-            $("#save-btn").on("click", function(e) {
-                // save to local storage! 
-                var storedObj = {};
-                storedObj.name = placeObject.name; 
-                storedObj.address = placeObject.address;
-                storedObj.distance = infObject.distance;
-                storedObj.method = userMethod;
 
-                savedUserInput.push(storedObj);
-                updateLocalStorage("history", JSON.stringify(savedUserInput));
-            })
-            
+          // on click save btn to update local storage
+          $("#save-btn").on("click", function (e) {
+            // save to local storage!
+            var storedObj = {};
+            storedObj.name = placeObject.name;
+            storedObj.address = placeObject.address;
+            storedObj.distance = infObject.distance;
+            storedObj.method = userMethod;
 
-        // savedUserInput.push(storedObj);
-        // updateLocalStorage("history", JSON.stringify(savedUserInput));
+            savedUserInput.push(storedObj);
+            updateLocalStorage("history", JSON.stringify(savedUserInput));
+          });
+
+          // savedUserInput.push(storedObj);
+          // updateLocalStorage("history", JSON.stringify(savedUserInput));
 
           // get map and display content
-          fetchStatic(userLat, userLon, zoomValue, locationLat, locationLng);
+          fetchStatic(ranLocation, locationObject, zoomValue);
           displayContent(placeObject, infObject);
         });
       } else {
@@ -192,28 +203,23 @@ function distanceMatrixApi(
 }
 
 // PLACES API Function
-function placesApi(
-  userDistance,
-  userMethod,
-  userDestination,
-  userLat,
-  userLon
-) {
-    fetch(
-        "https://api.allorigins.win/raw?url=" +
-        encodeURIComponent(
-            "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-            userLat +
-            "," +
-            userLon +
-            "&radius=" +
-            userDistance +
-            "&type=" +
-            userDestination +
-            "&opennow=true&key=AIzaSyBNemHqQ_a0mlEfgAo0C2IZN3hwCYT4RDo"
-        )
-    ).then(function (response) {
-    if (response.ok) {
+function placesApi(locationObject) {
+  fetch(
+    "https://api.allorigins.win/raw?url=" +
+      encodeURIComponent(
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+          locationObject.lat +
+          "," +
+          locationObject.lon +
+          "&radius=" +
+          locationObject.distance +
+          "&type=" +
+          locationObject.destination +
+          "&opennow=true&key=AIzaSyBNemHqQ_a0mlEfgAo0C2IZN3hwCYT4RDo"
+      )
+  )
+    .then(function (response) {
+      if (response.ok) {
         response.json().then(function (data) {
           var randomResults =
             data.results[Math.floor(Math.random() * data.results.length)];
@@ -226,21 +232,17 @@ function placesApi(
             address: randomResults.vicinity,
             //photo: randomResults.photos[0].photo_reference,
             status: randomResults.opening_hours.open_now,
-            type: userDestination
-        };
+            type: locationObject.destination,
+          };
 
-        
-        // send photo ref to get photo
-        //photoFetch(placeObject.photo);
+          // send photo ref to get photo
+          //photoFetch(placeObject.photo);
 
           var locationLat = randomResults.geometry.location.lat;
           var locationLng = randomResults.geometry.location.lng;
           distanceMatrixApi(
-            locationLat,
-            locationLng,
-            userLat,
-            userLon,
-            userMethod,
+            { lat: locationLat, lng: locationLng },
+            locationObject,
             placeObject
           );
         });
@@ -297,26 +299,29 @@ $("#submit").on("click", function (e) {
   // package variables to send
   var userLat = userLocation.lat;
   var userLon = userLocation.lon;
-  placesApi(userDistance, userMethod, userDestination, userLat, userLon);
+  placesApi({
+    distance: userDistance,
+    method: userMethod,
+    destination: userDestination,
+    lat: userLat,
+    lon: userLon,
+  });
 });
 
-
-
-
-var outerCard = document.createElement("div"); 
-outerCard.className = "col s2"; 
+var outerCard = document.createElement("div");
+outerCard.className = "col s2";
 var secondCard = document.createElement("div");
-secondCard.className = "card blue-grey"; 
+secondCard.className = "card blue-grey";
 var thirdCard = document.createElement("div");
-thirdCard.className = "card-content white-text"; 
+thirdCard.className = "card-content white-text";
 var cardTitle = document.createElement("span");
-cardTitle.className = "card-title"; 
+cardTitle.className = "card-title";
 cardTitle.innerText = "Card Header";
-var cardIcon = document.createElement("i"); 
+var cardIcon = document.createElement("i");
 cardIcon.className = "material-icons";
 cardIcon.innerHTML = "add";
-var cardAddress = document.createElement("p"); 
-cardAddress.innerHTML = "1422 Fettler Way, Winter Garden FL 32787"; 
+var cardAddress = document.createElement("p");
+cardAddress.innerHTML = "1422 Fettler Way, Winter Garden FL 32787";
 
 thirdCard.appendChild(cardTitle);
 thirdCard.appendChild(cardIcon);
